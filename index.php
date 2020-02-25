b4702b53581b766f15cc57dfcd7c1b3890db4f04
Stiexeno <danka9536@gmail.com>
2020-02-25 12:45:48 +0200

    Added cars

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <title>SPEED & POWER</title>
 </head>
 <body>
-
+<div>
+    <img src="https://www.dacia.lt/CountriesData/Lithuania/images/cars/sandero2017/dacia-sandero-v2_ig_w600_h337.jpg" alt="">
+    <img src="https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/640x400/quality/80/https://s.aolcdn.com/commerce/autodata/images/USC80FOC051A021001.jpg" alt="">
+</div>
 </body>
 </html>
