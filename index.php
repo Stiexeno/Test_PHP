757576feb9474bd2e773f5eead3df15a7ac06d50
Mykolas <mykolas.be@gmail.com>
2020-02-25 13:09:25 +0200

    veikiantis page

--- index.php
+++ index.php
@@ -2,9 +2,34 @@
 
 ?>
 
+<<<<<<< HEAD
 <html>
     <body>
     <h1><?php print 'Boring content'; ?></h1>
     </body>
 </html>
 
+=======
+
+<html>
+<style>
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color:black;
+    }
+
+</style>
+<head>
+    <title>SPEED & POWER</title>
+</head>
+<body>
+<div>
+    <img src="https://www.dacia.lt/CountriesData/Lithuania/images/cars/sandero2017/dacia-sandero-v2_ig_w600_h337.jpg" alt="">
+    <img src="https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/640x400/quality/80/https://s.aolcdn.com/commerce/autodata/images/USC80FOC051A021001.jpg" alt="">
+</div>
+</body>
+</html>
+>>>>>>> dev
